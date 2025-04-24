0bbc10679a74435ac77e1f19d79c4e5017846b2d
danik <daniyarzhalgas05@gmail.com>
2025-04-24 17:10:51 +0500

    added serializer validator

--- products/serializers.py
+++ products/serializers.py
@@ -16,6 +16,28 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'description', 'category', 'category_id', 'image', 'is_active',
-                  'discount_percent',
-                  'discounted_price']
+        fields = [
+            'id', 'name', 'price', 'description',
+            'category', 'category_id', 'image',
+            'is_active', 'discount_percent', 'discounted_price'
+        ]
+
+    # Валидация имени
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Имя не может быть пустым.")
+        if len(value) < 3:
+            raise serializers.ValidationError("Имя должно быть не короче 3 символов.")
+        return value
+
+    # Валидация цены
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Цена должна быть больше 0.")
+        return value
+
+    # Валидация процента скидки
+    def validate_discount_percent(self, value):
+        if not (0 <= value <= 100):
+            raise serializers.ValidationError("Скидка должна быть от 0 до 100.")
+        return value
